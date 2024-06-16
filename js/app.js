3fe4328c123a955cb473cffa883d0bb5c6dadc41
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-16 16:24:08 -0700

    :brick: take a step back :rewind:

--- js/app.js
+++ js/app.js
@@ -13,46 +13,6 @@ const mainSections = document.querySelectorAll("main section"); // get all secti
  *
  */
 
-/// ---------------------------------------------------------------------------------
-//  -- Create a main section HTML Fragment
-function createSectionFragment() {
-  const sectionFragment = `<h2>Winning Play</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Sed convallis sollicitudin mauris ac tincidunt. 
-    Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, 
-    ut pulvinar quam nunc eu augue. 
-    Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. 
-    Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. 
-    Phasellus imperdiet porta orci eget mollis.  Morbi a tincidunt felis. 
-    Sed leo nunc, pharetra et elementum non, faucibus vitae elit. 
-    Integer nec libero venenatis libero ultricies molestie semper in tellus. 
-    Sed congue et odio sed euismod. Pellentesque maximus imperdiet elit a pharetra. 
-    Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus.</p>
-
-    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, 
-    ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, 
-    vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet 
-    porttitor tortor, eget elementum tortor mollis non.</p>`;
-
-  return sectionFragment;
-}
-/// ---------------------------------------------------------------------------------
-// create winning__play section
-function createWinningPlaySection() {
-  const winningPlaySection = document.createElement("section");
-  winningPlaySection.id = "winning__play";
-  winningPlaySection.dataset.nav = "Winning Play";
-  // create div in section
-  const winningPlayDiv = document.createElement("div");
-  winningPlayDiv.classList.add("landing__container");
-  winningPlayDiv.innerHTML = createSectionFragment();
-  winningPlaySection.append(winningPlayDiv);
-  mainMain.append(winningPlaySection);
-}
-
-/// ---------------------------------------------------------------------------------
-//  -- this will add another section if uncommented.
-// createWinningPlaySection();
 
 /**
  * End Helper Functions
