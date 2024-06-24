cdfd4aad5c8e7101983e0ceefec6061b01f974b7
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-23 17:01:47 -0700

    :beers: :wheelchair: :bento:

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const pageHeader = document.querySelector(".page__header")
 const pageNav = document.querySelector(".page__nav");
 const mainEl = document.querySelector(".page__main");
-const modal = document.querySelector(".modal-container");
+// const modal = document.querySelector(".modal-container");
 const container = document.querySelector(".container");
 let isHovering = false;
 let isScrolling = false;
@@ -14,7 +14,7 @@ navList.className = "nav-list";
 const goToTopButton = document.createElement("button");
 goToTopButton.textContent= "Go to Top";
 goToTopButton.className = "to-top-btn"
-container.insertAdjacentElement("afterend", goToTopButton)
+mainEl.insertAdjacentElement("afterend", goToTopButton)
 
 // Retrieves all section elements within the main content area
 function getSections(){
@@ -48,6 +48,8 @@ function createNavList(){
 }
 createNavList();
 
+
+
 // Adds click event listener to navigation list for smooth scrolling
 navList.addEventListener("click", scroll);
 
@@ -70,8 +72,6 @@ function scroll(event){
             behavior: "smooth",
         });
         target.parentElement.classList.add("active");
-
-        
     }
 }
 
@@ -110,21 +110,19 @@ function getNavItem(id){
     }
 }
 
+// // Adds a new section to the main content area and updates the navigation list
+// const newSection = document.createElement("section");
+// newSection.id="section4";
+// newSection.dataset["nav"] = "Section 4";
+// newSection.innerHTML = ` <div class="landing__container">
+// <h2>Section 4</h2>
+// <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
+// <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+// </div>`
 
-// Adds a new section to the main content area and updates the navigation list
-const newSection = document.createElement("section");
-newSection.id="section4";
-newSection.dataset["nav"] = "Section 4";
-newSection.innerHTML = ` <div class="landing__container">
-<h2>Section 4</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-
-<p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-</div>`
-
-// Section creation and appending omitted for brevity
-mainEl.appendChild(newSection)
+// // Section creation and appending omitted for brevity
+// mainEl.appendChild(newSection)
 createNavList()
 
 // Handles the "Go to Top" button's click event for smooth scrolling to the top of the page
@@ -150,51 +148,50 @@ function addBackToTopButton(){
     }
 }
 
-// Opens a modal for adding a new section to the page
-// Opens a modal for adding a new section to the page
-function openAddSection(){
+// // Opens a modal for adding a new section to the page
+// function openAddSection(){
     
-    pageHeader.classList.add("hidden");
-    document.removeEventListener("scroll", makeActive);
-    modal.style.visibility = "visible";
-    modal.style.opacity = "1";
-}
+//     pageHeader.classList.add("hidden");
+//     document.removeEventListener("scroll", makeActive);
+//     modal.style.visibility = "visible";
+//     modal.style.opacity = "1";
+// }
 
-// Creates a new section based on user input from the modal
-function createSection(event){
-    const start = performance.now();
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const sectionName = formData.get("sectionName");
-    const sectionContent = formData.get("sectionContent").split("\n");
-    const content = sectionContent.map((item) =>  (item !== "") ? `<p>${item}</p>` : null)
+// // Creates a new section based on user input from the modal
+// function createSection(event){
+//     const start = performance.now();
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const sectionName = formData.get("sectionName");
+//     const sectionContent = formData.get("sectionContent").split("\n");
+//     const content = sectionContent.map((item) =>  (item !== "") ? `<p>${item}</p>` : null)
 
-    const idNumber = parseInt(navList.lastChild.children[0].href.split("#")[1].split("section")[1]) + 1
-    const newSection = document.createElement("section");
-    newSection.id="section" + idNumber;
-    newSection.dataset["nav"] = sectionName || "Section " + idNumber;
-    newSection.innerHTML = ` <div class="landing__container">
-    <h2>${sectionName}</h2>${content.join("") }</div>`;
+//     const idNumber = parseInt(navList.lastChild.children[0].href.split("#")[1].split("section")[1]) + 1
+//     const newSection = document.createElement("section");
+//     newSection.id="section" + idNumber;
+//     newSection.dataset["nav"] = sectionName || "Section " + idNumber;
+//     newSection.innerHTML = ` <div class="landing__container">
+//     <h2>${sectionName}</h2>${content.join("") }</div>`;
 
-    mainEl.appendChild(newSection);
-    createNavList()
-    event.target.reset();
-    modal.style.visibility = "hidden";
-    modal.style.opacity = "0";
-    document.addEventListener("scroll", makeActive);
-    pageHeader.classList.remove("hidden");
+//     mainEl.appendChild(newSection);
+//     createNavList()
+//     event.target.reset();
+//     modal.style.visibility = "hidden";
+//     modal.style.opacity = "0";
+//     document.addEventListener("scroll", makeActive);
+//     pageHeader.classList.remove("hidden");
 
-    console.log("Performance: " , performance.now() - start);
-}
+//     console.log("Performance: " , performance.now() - start);
+// }
 
-// Closes the modal and re-enables dynamic section highlighting
-const closeModal = document.querySelector(".close-modal");
-closeModal.addEventListener("click", () =>{
-    modal.style.visibility = "hidden";
-    modal.style.opacity = "0";
-    pageHeader.classList.remove("hidden");
-    document.addEventListener("scroll", makeActive);
-})
+// // Closes the modal and re-enables dynamic section highlighting
+// const closeModal = document.querySelector(".close-modal");
+// closeModal.addEventListener("click", () =>{
+//     modal.style.visibility = "hidden";
+//     modal.style.opacity = "0";
+//     pageHeader.classList.remove("hidden");
+//     document.addEventListener("scroll", makeActive);
+// })
 
 function hideHeader(){
     if(!isHovering  && !pageHeader.classList.contains("hidden")){pageHeader.classList.add("hidden")}
@@ -204,7 +201,6 @@ function showHeader(){
     pageHeader.classList.remove("hidden");
 }
 
-// Hides the page header after a delay when scrolling stops
 // Hides the page header after a delay when scrolling stops
 document.addEventListener("scrollend", (e) =>{
     if(isScrolling){
